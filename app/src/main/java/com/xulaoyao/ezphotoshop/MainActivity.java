3a08a2bb366fa673ee8e8c0cc294bf083f084f6c
renpeng <renpeng@jxfls.com>
2018-03-18 21:20:58 +0800

    first commit

---
+++ app/src/main/java/com/xulaoyao/ezphotoshop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xulaoyao.ezphotoshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
